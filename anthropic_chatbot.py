7e34b2c102877d63035f0d785721408d0f0fcc5c
shubham rajput <20janshubham2000@gmail.com>
2024-11-01 15:53:31 +0530

    rmv unnecessarry imports

--- anthropic_chatbot.py
+++ anthropic_chatbot.py
@@ -4,7 +4,6 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 import os
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
